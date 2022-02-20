456dc01253d0d6b0c6ed2c5db51c47ecf8d66a15
shahanaz.sultana <shahanaz.sultana@datasoft-bd.com>
2022-02-19 23:21:09 -0800

    First Commit

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+//import IpfsInfuraNFTCollectionUpload from './IpfsInfuraNFTCollectionUpload';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+    {/* <IpfsInfuraNFTCollectionUpload/> */}
     <App />
   </React.StrictMode>,
   document.getElementById('root')
